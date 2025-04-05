bcb7d69607a69fcfb2f6b5273796322ce78d8b5d
KomalNagdev <nagdev.komal37@gmail.com>
2025-04-05 17:35:41 +0530

    custom  for Yajra Data Tables usin livewire

--- app/Livewire/UsersData.php
+++ app/Livewire/UsersData.php
@@ -2,12 +2,62 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
+use Livewire\Attributes\Url;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class UsersData extends Component
 {
+    use WithPagination;
+
+    #[Url()]
+    public $perPage = 5;
+
+    #[Url(history:true)]
+    public $search = "";
+
+    #[Url(history:true)]
+    public $userType = "";
+
+    #[Url(history:true)]
+    public $sortBy = "created_at";
+
+    #[Url(history:true)]
+    public $sortByDirection = "DESC";
+
+    public function updatedSearch() {
+        $this->resetPage();
+    }
+
+    public function delete(User $user){
+        $user->delete();
+    }
+
+    public function setSortBy($sortByField) {
+
+        if($this->sortBy === $sortByField) {
+            $this->sortByDirection = ($this->sortByDirection == "ASC") ? "DESC" : "ASC";
+            return;
+        }
+
+        $this->sortBy = $sortByField;
+        $this->sortByDirection = 'DESC';
+
+    }
+
     public function render()
     {
-        return view('livewire.users-data');
+        return view(
+            'livewire.users-data',
+            [
+                'users' => User::search($this->search)
+                    ->when($this->userType !== '', function($query){
+                        $query->where('is_admin', $this->userType);
+                    })
+                    ->orderBy($this->sortBy, $this->sortByDirection)
+                    ->paginate($this->perPage),
+            ]
+        );
     }
 }
